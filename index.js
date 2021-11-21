c71dc960ca3cef494e01923f038b63577be00137
Lionel Mason <capitalcookhouse@gmail.com>
2021-11-21 11:09:13 -0500

    commit tree 3265e84ac6dde1b258fd8db990e5f0fbb723cb3b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching (drivers, name){
+//     return drivers.filter(driver => name.toLowerCase() == driver.toLowerCase())
+// }
+//    function fuzzyMatch (drivers, name){
+//        return drivers.filter(driver => name.toLowerCase() == drivers.indexOf(1, 2, 4))
+//    }
+
+//    function matchName (drivers, object){
+//        return drivers.filter(driver => object.name == object.hometown)
+//    }
+
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
